7c86b54fd61c2565e3153a0a7e2f0743b543ea68
jeppe314 <jesperlindqvist93@gmail.com>
2022-10-22 13:42:28 +0200

    Hi

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,15 +1,15 @@
-import React, { createRef, useState, useEffect } from "react"
+import React, { createContext, useState, useEffect } from "react"
 import { auth } from "../firebase"
 import { onAuthStateChanged } from "firebase/auth"
 
-export const AuthContext = ({ children }) => {
-  const AuthContext = createRef()
+export const AuthContext = createContext()
+
+export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState({})
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user)
-      console.log(user)
     })
 
     return () => {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,17 @@
-import React from "react"
+import React, { useContext } from "react"
 import { doc, getDoc } from "firebase/firestore"
-import { db } from "/firebase"
+import { db, auth } from "../firebase"
 import { AuthContext } from "../context/AuthContext"
 
 export const Home = () => {
+  const { currentUser } = useContext(AuthContext)
+  const { displayName, email } = currentUser
   const docRef = doc(db, "users", "SF")
 
   return (
     <div className="formContainer">
       <div className="homeWrapper">
-        <h2>Hello, Name!</h2>
+        <h2>Hi there, {displayName}!</h2>
         <button>Log Out</button>
       </div>
     </div>
